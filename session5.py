549ac599e406d0fb077376979a9eb30d6e75b6a0
Gajanana Ganjigatti <40160330+Gaju27@users.noreply.github.com>
2020-08-17 23:02:53 +0530

    Add files via upload

--- session5.py
+++ session5.py
@@ -1,6 +1,103 @@
+import math
 import random
-from math import hypot
-
-import self as self
+import time
 
 
+def time_it(fn, *args, repetitons=1, **kwargs):
+    if repetitons <= 0:
+        raise ValueError("Repetions must be greater than 0 value")
+    if fn == print:
+        t1 = time.perf_counter()
+        for p in range(repetitons):
+            print(*args, **kwargs)
+        t2 = time.perf_counter()
+    elif fn == 'squared_power_list':
+        t1 = time.perf_counter()
+        output = squared_power_list(*args, **kwargs)
+        t2 = time.perf_counter()
+    elif fn == 'polygon_area':
+        '''where
+        s  is the length of any side
+        n  is the number of sides
+        tan  is the tangent function calculated in degrees
+        time_it(polygon_area, 15, sides = 3, repetitons=10)'''
+        t1 = time.perf_counter()
+        output = polygon_area(*args, **kwargs)  # (s * s * n) / (4 * math.tan(180 / n))
+        t2 = time.perf_counter()
+    elif fn == 'temp_converter':
+        t1 = time.perf_counter()
+        output = temp_converter(*args, **kwargs)
+        t2 = time.perf_counter()
+    return ((t2 - t1) / repetitons, output)
+
+
+def squared_power_list(*args, **kwargs):
+    pvalue = args[0]
+    if pvalue <= 0:
+        raise ValueError("Please provide Power value must be greater than 0")
+    start, end = kwargs.values()
+    list_of_squares = [pvalue ** i for i in range(start, end + 1)]
+    return list_of_squares
+
+
+def polygon_area(*args, **kwargs):
+    length = args[0]
+    nsides = kwargs['sides']
+    if nsides == 3:
+        ''' Heron's Formula sometimes called Hero's formula
+            Using which we can calculate area of triagle when 
+            length of side knows to us '''
+        s = (length + length + length) / 2
+        area = math.sqrt(s * (s - length) * (s - length) * (s - length))
+
+    elif nsides == 4:
+        area = length * length
+    elif nsides == 5:
+        ''' when all the side length is equal
+            divide the pentagon into five triagle
+            draw a center line with 90degree
+            base of triagle is half of the side length of pentagon
+            hence angle of pentagon center is 36 degree (i.e 360/10=36)
+            tan(36)=(pentagon_length/2)/adjacent
+            adjacent= (pentagon_length/2)/tan(36)            
+            area_triangle=1/2(base_of_triagle * adjacent)
+            area_pentagon=10* area_triagle'''
+        base_of_triangle = length / 2
+        adjacent = base_of_triangle / math.tan(36)
+        area_triangle = (base_of_triangle * adjacent) / 2
+        area = 10 * area_triangle
+    elif nsides == 6:
+        # area = (length * length * nsides) / (4 * math.tan(180 / nsides))
+        area = ((3 * math.sqrt(3)) / 2) * (length ** 2)
+    else:
+        raise ValueError("Must be in range of 3 to 6 .")
+    return area
+
+
+def temp_converter(*args, **kwargs):
+    current_temp = args[0]
+    input_temp, output_temp = kwargs.values()
+    if input_temp=='f':
+        if output_temp=='c':
+            final_temp= (current_temp -32) * (5/9)
+        elif output_temp=='k': # k for kelvin
+            final_temp = (current_temp -32) * (5/9) +273.15
+        else:
+            raise Exception ("Not in any conversion key value must in 'c' or 'k' . ")
+    elif input_temp == 'c':
+        if output_temp == 'f':
+            final_temp = (current_temp * 9/5) +32
+        elif output_temp == 'k':
+            final_temp= current_temp + 273.15
+        else:
+            raise Exception("Not in any conversion key value must in 'f' or 'k' ")
+    elif input_temp == 'k':
+        if output_temp == 'c':
+            final_temp = (current_temp - 273.15 )
+        elif output_temp == 'f':
+            final_temp= (current_temp - 273.15 ) * (9/5) +32
+        else:
+            raise Exception("Not in any conversion key value must in 'f' or 'c' ")
+    else:
+        raise Exception("Not in standard temperature input")
+    return final_temp
--- test_session5.py
+++ test_session5.py
@@ -39,7 +39,7 @@ def test_readme_file_for_formatting():
     f = open("README.md", "r")
     content = f.read()
     f.close()
-    assert content.count("#") >= 10
+    assert content.count("#") >= 5
 
 def test_indentations():
     ''' Returns pass if used four spaces for each level of syntactically \
@@ -50,3 +50,74 @@ def test_indentations():
         assert len(space) % 4 == 2, "Your script contains misplaced indentations"
         assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines"
 
+def test_function_name_had_cap_letter():
+    functions = inspect.getmembers(session5, inspect.isfunction)
+    for function in functions:
+        assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
+
+def test_print_repetitons_zero_and_negative():
+    with pytest.raises(ValueError) as e_info:
+        session5.time_it(print, 1, 2, 3, sep='-', end= ' ***\n', repetitons=-1)
+    with pytest.raises(ValueError) as e_info:
+        session5.time_it(print, 1, 2, 3, sep='-', end= ' ***\n', repetitons=0)
+
+def test_invalid_repetion_valueerror_provides_relevant_message():
+    with pytest.raises(ValueError, match=r".* greater than 0 .*"):
+        session5.time_it(print, 1, 2, 3, sep='-', end= ' ***\n', repetitons=-1)
+
+def test_squared_power_list_assertion():
+    assert session5.squared_power_list(2, start=0, end=5) == [1, 2, 4, 8, 16, 32]
+
+def test_squared_power_list_assertion_for_only_negetive_start():
+    assert session5.squared_power_list(2,start=-5, end=3) == [0.03125, 0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8]
+
+def test_squared_power_list_assertion_for_only_negetive_end():
+    assert session5.squared_power_list(2,start=0, end=-3) == []
+
+def test_squared_power_list_assertion_for_negetive_start_end():
+    assert session5.squared_power_list(2,start=-6, end=-3) == [0.015625, 0.03125, 0.0625, 0.125]
+
+
+def test_proper_number_square_power_list():
+    with pytest.raises(ValueError):
+        session5.squared_power_list(-1, start=0, end=5)
+
+def test_invalid_number_square_power_list_relevant_message():
+    with pytest.raises(ValueError, match=r".* Power value .*"):
+        session5.time_it('squared_power_list', -1, start=0, end=5, repetitons=5)
+    with pytest.raises(ValueError, match=r".* Power value .*"):
+        session5.time_it('squared_power_list', 0, start=0, end=5, repetitons=5)
+
+def test_invalid_range_valueerror_provides_relevant_message():
+    with pytest.raises(ValueError, match=r".* range of 3 to 6.*"):
+        session5.time_it("polygon_area", 15, sides=2, repetitons=10)
+
+def test_invalid_side_for_polygon():
+    with pytest.raises(ValueError):
+        session5.time_it("polygon_area", 15, sides=7, repetitons=10)
+    with pytest.raises(ValueError):
+        session5.time_it("polygon_area", 15, sides=0, repetitons=10)
+
+def test_polygon_area_assertion_for_triangle():
+    assert session5.polygon_area(15, sides=3) == 97.42785792574935
+
+def test_polygon_area_assertion_for_square():
+    assert session5.polygon_area(15, sides=4) == 225
+
+def test_polygon_area_assertion_for_pentagon():
+    assert session5.polygon_area(15, sides=5) == 36.28811764110858
+
+def test_polygon_area_assertion_for_hexagon():
+    assert session5.polygon_area(15, sides=6) == 584.5671475544962
+
+def test_invalid_temp_input_provides_relevant_message():
+    with pytest.raises(Exception, match=r".* standard temperature .*"):
+        session5.time_it("temp_converter", 100, temp_given_in='g',convert_temp='k', repetitons=100)
+
+def test_invalid_temp_output_provides_relevant_message():
+    with pytest.raises(Exception, match=r".* value must in 'c' or 'k' .*"):
+        session5.time_it("temp_converter", 100, temp_given_in='f',convert_temp='g', repetitons=100)
+    with pytest.raises(Exception, match=r".* value must in 'f' or 'k' .*"):
+        session5.time_it("temp_converter", 100, temp_given_in='c',convert_temp='g', repetitons=100)
+    with pytest.raises(Exception, match=r".* value must in 'f' or 'c' .*"):
+        session5.time_it("temp_converter", 100, temp_given_in='k',convert_temp='g', repetitons=100)
